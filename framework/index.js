f1186a19d35eba8cd1e9c66d0ba23623718039ff
elvinciqueira <elvinciqueira@gmail.com>
2021-11-02 16:02:53 -0300

    feat: change detection

--- framework/index.js
+++ framework/index.js
@@ -2,7 +2,41 @@ import { init, eventListenersModule } from 'snabbdom';
 const patch = init([eventListenersModule]);
 
 export const render = (selector, component) => {
-  console.log('component', component);
   const $app = document.querySelector(selector);
   patch($app, component.template);
+};
+
+let state = {};
+
+export const createComponent = ({
+  template,
+  methods = {},
+  initialState = {},
+}) => {
+  state = initialState;
+  let previous;
+
+  const mappedMethods = (props) =>
+    Object.keys(methods).reduce(
+      (acc, key) => ({
+        ...acc,
+        [key]: (...args) => {
+          state = methods[key](state, ...args);
+          const nextNode = template({
+            ...props,
+            ...state,
+            methods: mappedMethods(props),
+          });
+          patch(previous.template, nextNode.template);
+          previous = nextNode;
+          return state;
+        },
+      }),
+      {}
+    );
+
+  return (props) => {
+    previous = template({ ...props, ...state, methods: mappedMethods(props) });
+    return previous;
+  };
 };
